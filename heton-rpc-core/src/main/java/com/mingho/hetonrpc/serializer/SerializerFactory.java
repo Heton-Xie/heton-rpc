47074bee66a73da912f699be4d7c71fd65c27650
Heton <121666875@qq.com>
2024-03-31 20:45:02 +0800

    refactor: 以 SpiLoader 加载序列化器的方法重构 SerializerFactory

--- heton-rpc-core/src/main/java/com/mingho/hetonrpc/serializer/SerializerFactory.java
+++ heton-rpc-core/src/main/java/com/mingho/hetonrpc/serializer/SerializerFactory.java
@@ -1,34 +1,27 @@
 package com.mingho.hetonrpc.serializer;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import com.mingho.hetonrpc.spi.SpiLoader;
 
 /**
  * 序列化器工厂（工厂模式，用于获取序列化器对象）
  */
 public class SerializerFactory {
 
-    /**
-     * 序列化映射，用于实现单例
-     */
-    private static final Map<String, Serializer> KEY_SERIALIZER_MAP = new HashMap<String, Serializer>(){{
-        put(SerializerKeys.JDK, new JdkSerializer());
-        put(SerializerKeys.JSON, new JsonSerializer());
-        put(SerializerKeys.KRYO, new KryoSerializer());
-        put(SerializerKeys.HESSIAN, new HessianSerializer());
-    }};
+    static {
+        SpiLoader.load(Serializer.class);
+    }
 
     /**
      * 默认序列化器
      */
-    private static final Serializer DEFAULT_SERIALIZER = KEY_SERIALIZER_MAP.get(SerializerKeys.JDK);
+    private static final Serializer DEFAULT_SERIALIZER = new JdkSerializer();
 
     /**
      * 根据 key 获取对应的序列化器
      */
     public static Serializer getInstance(String key){
-        return KEY_SERIALIZER_MAP.getOrDefault(key, DEFAULT_SERIALIZER);
+        return SpiLoader.getInstance(Serializer.class, key);
     }
 
 }
