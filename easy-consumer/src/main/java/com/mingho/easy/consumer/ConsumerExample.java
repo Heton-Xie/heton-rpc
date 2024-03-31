3f69901f2fa8c227bb08b05cfbf72fe23ded5f92
Heton <121666875@qq.com>
2024-03-31 16:39:12 +0800

    feat: 完成 mock 接口的开发

--- easy-consumer/src/main/java/com/mingho/easy/consumer/ConsumerExample.java
+++ easy-consumer/src/main/java/com/mingho/easy/consumer/ConsumerExample.java
@@ -1,15 +1,29 @@
 package com.mingho.easy.consumer;
 
+import com.mingho.easy.common.model.User;
+import com.mingho.easy.common.service.UserService;
 import com.mingho.hetonrpc.RpcApplication;
 import com.mingho.hetonrpc.config.RpcConfig;
+import com.mingho.hetonrpc.proxy.ServiceProxyFactory;
 
 /**
  * 服务消费者示例
  */
 public class ConsumerExample {
     public static void main(String[] args) {
-        // 获取配置，有配置则按配置走，无配置则使用全局默认配置
-        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
-        System.out.println(rpcConfig);
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("heton");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+
+        long number = userService.getNumber();
+        System.out.println(number);
     }
 }
--- heton-rpc-core/src/main/java/com/mingho/hetonrpc/proxy/ServiceProxyFactory.java
+++ heton-rpc-core/src/main/java/com/mingho/hetonrpc/proxy/ServiceProxyFactory.java
@@ -1,5 +1,7 @@
 package com.mingho.hetonrpc.proxy;
 
+import com.mingho.hetonrpc.RpcApplication;
+
 import java.lang.reflect.Proxy;
 
 /**
@@ -10,14 +12,28 @@ public class ServiceProxyFactory {
     /**
      * 根据服务类获取代理对象
      *
-     * @param serviceClass
-     * @param <T>
-     * @return
+     * @param serviceClass 要实现的接口
      */
     public static <T> T getProxy(Class<T> serviceClass) {
+        if (RpcApplication.getRpcConfig().isMock()) {
+            return getMockProxy(serviceClass);
+        }
+
         return (T) Proxy.newProxyInstance(
                 serviceClass.getClassLoader(),
                 new Class[]{serviceClass},
                 new ServiceProxy());
+    }
+
+    /**
+     * 根据服务类获取 Mock 代理对象
+     *
+     * @param serviceClass 要实现的接口
+     */
+    public static <T> T getMockProxy(Class<T> serviceClass) {
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                new MockServiceProxy());
     }
 }
