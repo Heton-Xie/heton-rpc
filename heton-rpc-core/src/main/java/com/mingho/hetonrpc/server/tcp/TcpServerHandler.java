d8ab84099a22952ab45b03ae756c077dd399bd74
Heton <121666875@qq.com>
2024-04-07 15:41:50 +0800

    feat: 封装 TCP 请求处理器，并将服务器的处理器修改为新封装的处理器

---
+++ heton-rpc-core/src/main/java/com/mingho/hetonrpc/server/tcp/TcpServerHandler.java
@@ -0,0 +1,69 @@
+package com.mingho.hetonrpc.server.tcp;
+
+import com.mingho.hetonrpc.model.RpcRequest;
+import com.mingho.hetonrpc.model.RpcResponse;
+import com.mingho.hetonrpc.protocol.ProtocolMessage;
+import com.mingho.hetonrpc.protocol.ProtocolMessageDecoder;
+import com.mingho.hetonrpc.protocol.ProtocolMessageEncoder;
+import com.mingho.hetonrpc.protocol.ProtocolMessageTypeEnum;
+import com.mingho.hetonrpc.registry.LocalRegistry;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetSocket;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * TCP 请求处理器
+ */
+public class TcpServerHandler implements Handler<NetSocket> {
+
+    /**
+     * 处理请求
+     *
+     * @param netSocket 要处理的 socket
+     */
+    @Override
+    public void handle(NetSocket netSocket) {
+        // 处理连接
+        netSocket.handler(buffer -> {
+            // 接受请求，解码
+            ProtocolMessage<RpcRequest> protocolMessage;
+            try {
+                protocolMessage = (ProtocolMessage<RpcRequest>) ProtocolMessageDecoder.decode(buffer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            RpcRequest rpcRequest = protocolMessage.getBody();
+
+            // 处理请求
+            // 构造响应结果对象
+            RpcResponse rpcResponse = new RpcResponse();
+            try {
+                Class<?> implClass = LocalRegistry.get(rpcRequest.getServiceName());
+                Method method = implClass.getMethod(rpcRequest.getMethodName());
+                Object result = method.invoke(implClass.newInstance(), rpcRequest.getArgs());
+                rpcResponse.setData(result);
+                rpcResponse.setDataType(method.getReturnType());
+                rpcResponse.setMessage("ok");
+            } catch (Exception e) {
+                rpcResponse.setMessage(e.getMessage());
+                rpcResponse.setException(e);
+                e.printStackTrace();
+            }
+
+            // 发送响应，编码
+            ProtocolMessage.Header header = protocolMessage.getHeader();
+            header.setType((byte) ProtocolMessageTypeEnum.RESPONSE.getKey());
+            ProtocolMessage<RpcResponse> responseProtocolMessage = new ProtocolMessage<>(header, rpcResponse);
+            try {
+                Buffer encode = ProtocolMessageEncoder.encode(responseProtocolMessage);
+                netSocket.write(encode);
+            } catch (IOException e) {
+                throw new RuntimeException("协议消息编码错误");
+            }
+        });
+    }
+
+}
--- heton-rpc-core/src/main/java/com/mingho/hetonrpc/server/tcp/VertxTcpServer.java
+++ heton-rpc-core/src/main/java/com/mingho/hetonrpc/server/tcp/VertxTcpServer.java
@@ -19,16 +19,7 @@ public class VertxTcpServer {
         NetServer server = vertx.createNetServer();
 
         // 处理请求
-        server.connectHandler(socket -> {
-            // 处理连接
-            socket.handler(buffer -> {
-                // 处理接收到的字节数组
-                byte[] requestData = buffer.getBytes();
-                byte[] responseData = handleRequest(requestData);
-                // 发送响应
-                socket.write(Buffer.buffer(responseData));
-            });
-        });
+        server.connectHandler(new TcpServerHandler());
 
         // 启动 TCP 服务器并监听指定端口
         server.listen(port, result -> {
